06f6f5008957fce8fa5c09feb5655c267fa5d220
Paul0112 <177804960+Paul0112@users.noreply.github.com>
2026-09-15 22:01:38 -0300

    End hyperparameters tests and add noise notebook

---
+++ notebooks/utils.py
@@ -0,0 +1,104 @@
+# requeriments
+import sys
+import os
+root_path = os.path.abspath(os.path.join('..'))
+if root_path not in sys.path: sys.path.append(root_path)
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from tdv.denoise_utils import apply_vn, check_color, psnr
+from skimage.metrics import structural_similarity as ssim
+
+
+def plot_results(z, x_S, y=None, sigma=25, figsize= (18, 12)):
+
+    """
+    z: noisy image
+    x_S: denoised image
+    y: ground truth
+    """
+
+    z, x_S = np.squeeze(z), np.squeeze(x_S)
+    y = np.squeeze(y) if y is not None else None
+
+    c_axis = -1 if z.ndim == 3 else None
+    cmap = 'gray' if z.ndim == 2 else None
+
+    if y is not None:
+
+        fig, ax = plt.subplots(1, 3, sharex=True, sharey=True, figsize=figsize)
+
+        ax[0].imshow(np.clip(z, 0, 1), cmap=cmap)
+        ax[0].set_title(rf'z ($\sigma$={sigma})')
+        ax[0].set_xlabel(f'PSNR={psnr(z,y):.2f}dB, 'f'SSIM={ssim(z,y,data_range=1,channel_axis=c_axis):.4f}')
+        ax[1].imshow(np.clip(x_S, 0, 1), cmap=cmap)
+        ax[1].set_title('x_S')
+        ax[1].set_xlabel(f'PSNR={psnr(x_S,y):.2f}dB, 'f'SSIM={ssim(x_S,y,data_range=1,channel_axis=c_axis):.4f}')
+        ax[2].imshow(np.clip(y, 0, 1), cmap=cmap)
+        ax[2].set_title('y')
+
+    else:
+
+        fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15, 5))
+
+        ax[0].imshow(np.clip(z, 0, 1), cmap=cmap)
+        ax[0].set_title('Noisy')
+        ax[1].imshow(np.clip(x_S, 0, 1), cmap=cmap)
+        ax[1].set_title('Denoised')
+
+    plt.show()
+
+
+def fix_hyperparams(vn, param, param_value):
+    p = param.lower()
+    if p == 's': vn.set_end(param_value)
+    elif p == 'lambda': vn.lmbda.data.fill_(param_value)
+    else: vn.T.data.fill_(param_value)
+
+
+def test_hyperparams(param, param_values, vn, img, param_ref, sigma = 25):
+    y, _ = check_color(img)
+    # add noise
+    z = y + sigma / 255 * np.random.randn(*y.shape).astype(np.float32)
+    z_th = torch.from_numpy(np.transpose(z, (2, 0, 1))[None])
+
+    x_all_by_param = {} # each intermediate step (img)
+    x_psnr_param = []
+    z_psnr = psnr(z, y)
+
+    try:
+        with torch.no_grad():
+            for p in param_values:
+                # select the correct paramt o set
+                fix_hyperparams(vn=vn, param=param, param_value=p)
+
+                x_all = apply_vn(z_th, z_th, vn, sigma, sigma_ref=25)
+                # CPU arrays with shape (S + 1, H, W, C)
+                x_all_by_param[p] = np.stack([
+                    np.transpose(x[0].cpu().numpy(), (1, 2, 0))
+                    for x in x_all
+                ])
+                x_final = x_all_by_param[p][-1] # denoising result of steps
+                current_psnr = psnr(x_final, y)
+                x_psnr_param.append(current_psnr)
+                print(f'{param}={p} | PSNR={x_psnr_param[-1]:.2f} dB')
+    finally:
+        # return to optimum value
+        fix_hyperparams(vn=vn, param=param, param_value=param_ref)
+
+    return x_psnr_param, x_all_by_param, z_psnr, z, y
+
+
+def plot_hyperparams(param, param_values, param_ref, x_psnr_param, z_psnr=None):
+
+    plt.figure(figsize=(12, 5))
+    plt.plot(param_values, x_psnr_param, 'o-', label=r'$x_S$ PSNR')
+    if z_psnr is not None: plt.axhline(z_psnr, linestyle=':', color='gray', label='z PSNR')
+    plt.axvline(param_ref, linestyle='--', color='red', label=f'{param}={param_ref} (checkpoint)')
+    plt.title(rf'PSNR v/s {param} ($\sigma=25$)')
+    plt.xlabel(f'{param}')
+    plt.ylabel('PSNR (dB)')
+    plt.xticks(param_values)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
--- tdv/denoise_utils.py
+++ tdv/denoise_utils.py
@@ -1,22 +1,27 @@
+# requeriments
+import os
 import numpy as np
 import torch
-from pathlib import Path
-from . import model
+import numpy as np
+import torch
+from tdv import model
 
 # define the evaluation metric
 def psnr(x, y): 
     return 20*np.log10(1.0/np.sqrt(np.mean((x-y) ** 2)))
 
 # define the application of the VN
-def apply_vn(x_0, z, vn, sigma, sigma_ref=25):
+def apply_vn(x_0, z, vn, sigma, sigma_ref=25, scaled= True):
     # tranform to reference noise level
-    scale = sigma_ref/sigma
+    scale = 1
+    if scaled: scale = sigma_ref/sigma
     x = vn(x_0 * scale, z * scale)
     # convert back to original scale
     x = [j/scale for j in x]
     return x
 
 def check_color(raw_image):
+    # check color of the input image and slice the img if necesary (+3 channels)
     y = None
     color = None
     if raw_image.ndim == 2 or (raw_image.ndim == 3 and raw_image.shape[-1] == 1):
@@ -31,28 +36,27 @@ def check_color(raw_image):
 
     return y, color
 
-
 def load_model(color):
     # load the model state dict
-    checkpoint = torch.load(os.path.join('..', 'checkpoints', f'tdv3-3-25-f32-{color}.pth'), map_location=torch.device('cpu'))
+    checkpoint = torch.load(os.path.join('..', 'tdv/checkpoints', f'tdv3-3-25-f32-{color}.pth'), map_location=torch.device('cpu'))
 
     # get the variational network with the TDV regularizer
     vn = model.VNet(checkpoint['config'], efficient=False)
     vn.load_state_dict(checkpoint['model'])
+    #vn.eval()
 
     return vn, checkpoint
 
-def denoise_z(vn, z, sigma=25, sigma_ref=25):
+def denoise_z(vn, z, sigma=25, sigma_ref=25, scaled=True):
     z_th = torch.from_numpy(np.transpose(z, (2,0,1))[None])
     
     with torch.no_grad():
-        x_th = apply_vn(z_th, z_th, vn, sigma, sigma_ref)
+        x_th = apply_vn(z_th, z_th, vn, sigma, sigma_ref, scaled)
     x_S = np.transpose(x_th[-1][0].cpu().numpy(), (1,2,0))
 
     return x_S
 
-
-def test_img(vn, img, sigma, add_noise=True, noisy_img_path=None, n_bits = 8):
+def test_img(vn, img, sigma, add_noise=True, noisy_img_path=None, n_bits = 8, scaled= True):
     max_value = 2**n_bits - 1
     sigma_ref = 25
     img = img.astype(np.float32)
@@ -66,6 +70,6 @@ def test_img(vn, img, sigma, add_noise=True, noisy_img_path=None, n_bits = 8):
 
     # push the images to torch
     #y_th = torch.from_numpy(np.transpose(y, (2,0,1))[None])
-    x_S = denoise_z(vn, z, sigma, sigma_ref)
+    x_S = denoise_z(vn, z, sigma, sigma_ref, scaled)
 
     return z, x_S, y
